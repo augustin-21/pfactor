00a1e7519f1d9b113d9d58b561db34428d652e15
iinava <131179814+iinava@users.noreply.github.com>
2025-07-13 14:16:07 +0530

    feat: add login and logout views with token authentication for user management

--- core/users/views.py
+++ core/users/views.py
@@ -2,9 +2,30 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import UserRegistrationSerializer
 from django.contrib.auth.models import User
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import permissions, status
 
 # Create your views here.
 
 class UserRegistrationView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserRegistrationSerializer
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        return Response({'token': token.key, 'user_id': token.user_id, 'username': token.user.username})
+
+class LogoutView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        try:
+            request.user.auth_token.delete()
+        except (AttributeError, Token.DoesNotExist):
+            pass
+        return Response(status=status.HTTP_204_NO_CONTENT)
