8a5ef500e26ce1efe13924d6287efc9c814e33a5
iinava <131179814+iinava@users.noreply.github.com>
2025-07-13 14:06:19 +0530

    feat: implement user registration API with serializers and routing

--- core/users/views.py
+++ core/users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import UserRegistrationSerializer
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer
